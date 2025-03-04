f55ac40641391550df6d5dba4b615047532b4f18
Uday Bhardwaj <udaybh07@gmail.com>
2025-03-05 04:58:20 +0530

    Deployment 0.1

--- backend/index.js
+++ backend/index.js
@@ -1,104 +1,118 @@
-const express= require('express')
-const cors= require('cors')
+const express= require( 'express' )
+const morgan= require( 'morgan' )
+const cors= require('cors');
 
-// creating an instance of express
-const app= express()
-app.use( cors() )
-app.use( express.static('dist') )
-app.use( express.json() )
+const app= express();
+app.use( cors() );
+app.use( express.json() );
+app.use( morgan( ( tokens, req, res) => {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res( req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+  ].join(' ')
+}));
 
-let NOTES= [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: true
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: false
-  },
-  {
-    id: "4",
-    content: "Let us check if the post works",
-    important: true
-  },
-  {
-    id: "5",
-    content: "lets add another",
-    important: false
-  },
-  {
-    id: 6,
-    content: "lets add other",
-    important: true
-  },
-]
-
-
-const generateId = () => {
-  // note that array can not be directly given as a parameter to the Math.max method so spread ... syntax is used.
-  const maxId = NOTES.length > 0
-    ? Math.max( ...NOTES.map(n => Number(n.id)) )
-    : 0;
-    
-  return String(maxId + 1)
+const generateId= ()=>{
+  let val= Math.max( ...(persons.map( person => Number(person.id) ) ) )
+  return String(val+1);
 }
 
-// endpoints
-app.get( '/', ( req, res ) => {
-  res.send('<h1>Hello world</h1>')
-})
-
-app.get( '/api/notes', ( req, res ) => {
-  res.json(NOTES)
-})
-
-app.get( '/api/notes/:id', (req, res) =>{
-  const id= req.params.id
-  const note= NOTES.find( ( note ) => note.id==id )
-  if ( note ) {
-    res.json(note)
-  } 
-  else {
-    res.statusMessage= " The Note is not available."
-    res.status( 404 ).end()
+let persons= [
+  { 
+    id: "1",
+    name: "Arto Hellas", 
+    number: "040-123456"
+  },
+  { 
+    id: "2",
+    name: "Ada Lovelace", 
+    number: "39-44-5323523"
+  },
+  { 
+    id: "3",
+    name: "Dan Abramov", 
+    number: "12-43-234345"
+  },
+  { 
+    id: "4",
+    name: "Mary Poppendieck", 
+    number: "39-23-6423122"
   }
+]
+
+// get the main page of the application
+app.get( '/', ( req, res ) => {
+  res.send( '<h1>Phonebook</h1>' )
 })
 
-app.post( '/api/notes', ( req, res ) => {
+// get the json data of people in the phonebook
+app.get( '/api/persons', ( req, res ) => {
+  res.json( persons );
+})
 
+// getting the info about the phonebook
+app.get( '/info', ( req, res ) => {
+  const today= new Date()
+  const showDate=  today.toString(); 
+  
+  res.send(
+    `
+      <h1>Phonebook has ${persons.length} people.</h1>
+      <h1>${showDate}</h1>
+    `
+  )
+})
+
+// get an individual person with his id
+app.get( '/api/persons/:id', ( req, res ) => {
+  const id= req.params.id;
+  const person= persons.find( ( person ) => person.id===id )
+  if ( !person ) {
+    res.statusMessage= "Sorry, this person is not saved in the phonebook"
+    return res.status(404).end();
+  }
+  res.json( person );
+})
+
+// post the details of a new person
+app.post( '/api/persons', ( req, res ) => {
   const body= req.body;
-  if ( !body.content ) {
+  
+  if ( !( body.name && body.number ) ) {
     return res.status( 400 ).json(
-      { 'Error': 'Content is missing' }
+      { Error: 'Data is missing' }
     )
   }
 
-  const note= {
-    ...body,
-    content: body.content,
-    important: body.important || false,
-    id: generateId()
+  const isName= persons.find( ( person ) => person.name === body.name )
+  if ( isName ) {
+    return res.status( 400 ).json(
+      { Error: 'Name must not be same' }
+    )
   }
 
-  NOTES= NOTES.concat( note )
-  res.json( note );
+  const person= {
+    id: generateId(),
+    name: body.name,
+    number: body.number
+  }
+
+  persons= persons.concat( person )
+  res.json( person )
 })
 
-app.delete( '/api/notes/:id', ( req, res ) => {
+// deleting a particular person
+app.delete( '/api/persons/:id', ( req, res ) => {
   const id= req.params.id;
-  NOTES= NOTES.filter( ( note ) => note.id!==id )
+  persons= persons.filter( ( person ) => person.id!==id )
   res.status( 204 ).send()
 })
 
-const PORT= process.env.PORT || 3001
+// setting up the listening to the server
+const PORT=  process.env.PORT || 3001
 app.listen( PORT, () =>{
   console.log(`Server is running at Port ${PORT}`);
-  
 })
